831d567664cda4762d97dbae1e48348ae7c3e2c9
Dmitrii <dimterion59@gmail.com>
2026-03-21 10:15:44 +0100

    adjust variables for fetchStories function

--- src/services/storyService.js
+++ src/services/storyService.js
@@ -1,16 +1,17 @@
 import sampleStories from "../assets/sampleStories.json";
 import sampleStory from "../assets/sampleStory.json";
 
-// TO-DO: adjust both variables when API is ready.
+// TO-DO: adjust variables when API is ready.
 const USE_LOCAL_DATA = true;
-const API_URL = import.meta.env.VITE_STORIES_API_URL;
+const STORIES_API_URL = import.meta.env.VITE_STORIES_API_URL;
+const STORY_API_URL = import.meta.env.VITE_STORY_API_URL;
 
 export async function fetchStories(limit) {
   if (USE_LOCAL_DATA) return Promise.resolve(sampleStories.slice(0, limit));
 
-  const res = await fetch(`${API_URL}?limit=${limit}`);
+  const res = await fetch(`${STORIES_API_URL}?limit=${limit}`);
 
-  if (!res.ok) throw new Error("Could not fetch data.");
+  if (!res.ok) throw new Error("Could not fetch stories.");
 
   return res.json();
 }
@@ -20,13 +21,13 @@ export async function fetchStory(id) {
     const story = sampleStory[String(id)];
 
     if (!story) throw new Error("Story not found.");
-    
+
     return Promise.resolve(story);
   }
 
-  const res = await fetch(`${API_URL}/${id}`);
+  const res = await fetch(`${STORY_API_URL}/${id}`);
 
   if (!res.ok) throw new Error("Could not fetch story.");
-  
+
   return res.json();
 }
