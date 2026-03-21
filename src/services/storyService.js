0d2ece52cff8197dc090d00422c71f5a2e3bc3d6
Dmitrii <dimterion59@gmail.com>
2026-03-21 09:18:39 +0100

    adjust sample data structure

--- src/services/storyService.js
+++ src/services/storyService.js
@@ -1,11 +1,11 @@
-import sampleData from "../assets/sampleData.json";
+import sampleStories from "../assets/sampleStories.json";
 
 // TO-DO: adjust both variables when API is ready.
 const USE_LOCAL_DATA = true;
 const API_URL = import.meta.env.VITE_STORIES_API_URL;
 
 export async function fetchStories(limit) {
-  if (USE_LOCAL_DATA) return Promise.resolve(sampleData.slice(0, limit));
+  if (USE_LOCAL_DATA) return Promise.resolve(sampleStories.slice(0, limit));
 
   const res = await fetch(`${API_URL}?limit=${limit}`);
 
